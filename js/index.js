ca956c860e3a115f8619796c36b6fe65e7e649ab
Sushant <sushantshetty2001@gmail.com>
2021-09-24 19:09:21 +0530

    fourth commit

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 // console.log("Hello");
-
 // utility function to get dom element from string
 let getElementFromString = (string) => {
     let div = document.createElement('div');
@@ -9,10 +8,30 @@ let getElementFromString = (string) => {
 
 
 
+let requestJsonBox = document.getElementById('requestJsonBox');
+requestJsonBox.style.display = 'none';
+
 let parametersBox = document.getElementById('parametersBox');
 parametersBox.style.display = 'none';
 
+let headerBox = document.getElementById('headerBox');
+headerBox.style.display = 'none';
 
+
+
+
+// if user click on custom header
+let customheaderRadio = document.getElementById('customheaderRadio')
+customheaderRadio.addEventListener('click', () => {
+    document.getElementById('headerBox').style.display = 'block';
+    document.getElementById('tokenBox').style.display = 'none';
+})
+// if user click on default header header
+let defaultheaderRadio = document.getElementById('defaultheaderRadio')
+defaultheaderRadio.addEventListener('click', () => {
+    document.getElementById('headerBox').style.display = 'none';
+    document.getElementById('tokenBox').style.display = 'block';
+})
 
 
 // if user click on paramter
@@ -31,7 +50,50 @@ jsonRadio.addEventListener('click', () => {
 })
 
 
-// if + is clicked add parameters
+// if + is clicked add header
+let headerNumber = 0;
+let addHeaders = document.getElementById('addHeaders');
+addHeaders.addEventListener('click', () => {
+    let header = document.getElementById('header');
+    let string = `<div class="input-group  mt-2">
+                        
+                        <input
+                        type="text"
+                        class="form-control"
+                        id="headerKey${headerNumber + 2}"
+                        placeholder="Content-Type"
+                        />
+                        <input
+                        type="text"
+                        class="form-control ms-2"
+                        id="headerValue${headerNumber + 2}"
+                        placeholder="application/json"
+                        />
+                        <button
+                        type="button"
+                        class="btn btn-outline-secondary ms-2 removeHeader"
+                        data-bs-toggle="tooltip"
+                        data-bs-placement="top"
+                        title="Remove Header"
+                        >
+                        -
+                        </button>
+                </div>`;
+    // converting element string to dom
+    let headerElement = getElementFromString(string);
+    // console.log(parameterElement);
+    header.appendChild(headerElement);
+    // deleting parameters
+    let removeHeader = document.getElementsByClassName('removeHeader');
+    for (item of removeHeader) {
+        item.addEventListener('click', (e) => {
+            e.target.parentElement.remove();
+        })
+    }
+    headerNumber++;
+})
+
+// if + is clicked add parameter
 let parameterNumber = 0;
 let addParameters = document.getElementById('addParameters');
 addParameters.addEventListener('click', () => {
@@ -68,14 +130,9 @@ addParameters.addEventListener('click', () => {
     let removeParameters = document.getElementsByClassName('removeParameters');
     for (item of removeParameters) {
         item.addEventListener('click', (e) => {
-            let result = confirm("Want to delete?");
-            if (result) {
-                e.target.parentElement.remove();
-            }
+            e.target.parentElement.remove();
         })
     }
-
-
     parameterNumber++;
 })
 
@@ -87,13 +144,15 @@ submit.addEventListener('click', () => {
     document.getElementById('responsePrism').innerHTML = "Please wait.."
     // fetching all values
     let url = document.getElementById('url').value;
+    let token = document.getElementById('token').value;
     let requestType = document.querySelector("input[name='requestType']:checked").value;
     let contentType = document.querySelector("input[name='contentType']:checked").value;
-    //console.log(requestType);
+    let headerType = document.querySelector("input[name='headerType']:checked").value;
+    // console.log(headerType);
     // contenttype
     if (contentType == 'parameter') {
         data = {};
-        for (let i = 0; i < parameterNumber; i++) {
+        for (let i = 0; i < parameterNumber + 1; i++) {
             if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
                 let key = document.getElementById('parameterKey' + (i + 1)).value;
                 let value = document.getElementById('parameterValue' + (i + 1)).value;
@@ -105,13 +164,34 @@ submit.addEventListener('click', () => {
     }
     else {
         data = document.getElementById('requestJson').value;
+    }
 
+    if (headerType == 'customheader') {
+        header = {};
+        for (let i = 0; i < headerNumber + 1; i++) {
+            if (document.getElementById('headerKey' + (i + 1)) != undefined) {
+                let key = document.getElementById('headerKey' + (i + 1)).value;
+                let value = document.getElementById('headerValue' + (i + 1)).value;
+                // console.log(key,value)
+                header[key] = value;
+            }
+        }
+        // console.log(header);
+    }
+    else {
+        header = {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer ${token}`
+        }
+        // console.log(header)
     }
     //requestType
     if (requestType == 'GET') {
         fetch(url, {
-            method: 'GET'
-        })
+            method: 'GET',
+            headers: header,
+        }
+        )
             .then(resp => resp.text())
             .then((text) => {
                 // document.getElementById('responseJson').value = text
@@ -123,9 +203,7 @@ submit.addEventListener('click', () => {
         fetch(url, {
             method: 'POST',
             body: data,
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
+            headers: header
         })
             .then((response) => response.text())
             .then((text) => {
@@ -138,9 +216,7 @@ submit.addEventListener('click', () => {
         fetch(url, {
             method: 'PATCH',
             body: data,
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
+            headers:header
         })
             .then((response) => response.text())
             .then((text) => {
@@ -153,9 +229,7 @@ submit.addEventListener('click', () => {
         fetch(url, {
             method: 'PUT',
             body: data,
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
+            headers: header
         })
             .then((response) => response.text())
             .then((text) => {
@@ -167,14 +241,12 @@ submit.addEventListener('click', () => {
     else {
         fetch(url, {
             method: 'DELETE',
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
+            headers: header
         })
             .then(() => {
                 // console.log(text);
                 // document.getElementById('responseJson').value = text;
-                document.getElementById('responsePrism').innerHTML ="Deleted";
+                document.getElementById('responsePrism').innerHTML = "Deleted";
                 Prism.highlightAll();
             });
     }
